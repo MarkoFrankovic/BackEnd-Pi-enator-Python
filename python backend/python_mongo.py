29cc48bd515e8cdbc3a383373cdc57938c91ff7d
Marko <mfrankovi@student.unipu.hr>
2022-05-12 18:43:07 +0200

    Update python_mongo.py

--- python backend/python_mongo.py
+++ python backend/python_mongo.py
@@ -1,12 +1,12 @@
 import pymongo
-from flask import Flask
+import json
+from flask import Flask,jsonify
 app = Flask(__name__)
 
 myclient = pymongo.MongoClient(
     "mongodb+srv://Korisnik:korisnik@databaza.tip3k.mongodb.net/Databaza?retryWrites=true&w=majority")
 
 mydb = myclient["Pjesme"]
-#print(mydb.list_collection_names())
 
 Jaeger = mydb["Pjesme_Jaeger"]
 Bambus = mydb["Pjesme_Bambus"]
@@ -28,18 +28,42 @@ mydoc7 = Jack.find({},{ "_id": 0, "ime": 0, "ocjena": 0 }).sort("ocjena",-1)
 mydoc8 = Merlot.find({},{ "_id": 0, "ime": 0, "ocjena": 0 }).sort("ocjena",-1)
 mydoc9 = Stock.find({},{ "_id": 0, "ime": 0, "ocjena": 0 }).sort("ocjena",-1)
 
-#for x in mydoc1:
-    #print(x)
-
-
-@app.route('/')
-def hello_world():
-   return "Hello World"
-
 @app.route('/jaeger')
 def jaeger():
-   return mydoc1
-   
+   return jsonify(list(mydoc1))
+
+@app.route('/bambus')
+def bambus():
+   return jsonify(list(mydoc2))
+
+@app.route('/voda')
+def voda():
+   return jsonify(list(mydoc3))
+
+@app.route('/gin')
+def gin():
+   return jsonify(list(mydoc4))
+
+@app.route('/travarica')
+def travarica():
+   return jsonify(list(mydoc5))
+
+@app.route('/vodka')
+def vodka():
+   return jsonify(list(mydoc6))
+
+@app.route('/jack')
+def jack():
+   return jsonify(list(mydoc7))
+
+@app.route('/merlot')
+def merlot():
+   return jsonify(list(mydoc8))
+
+@app.route('/stock')
+def stock():
+   return jsonify(list(mydoc9))
 
 if __name__ == '__main__':
    app.run()
+
