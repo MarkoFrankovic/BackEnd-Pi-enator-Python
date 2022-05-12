6b6c3e84a325bbaea6df183de2d15b15b1bafcdd
Marko <mfrankovi@student.unipu.hr>
2022-05-12 11:26:03 +0200

    Update

--- python backend/python_mongo.py
+++ python backend/python_mongo.py
@@ -4,9 +4,9 @@ myclient = pymongo.MongoClient(
     "mongodb+srv://Korisnik:korisnik@databaza.tip3k.mongodb.net/Databaza?retryWrites=true&w=majority")
 
 mydb = myclient["Pjesme"]
-mycol = mydb["Pjesme_Bambus"]
+Bambus = mydb["Pjesme_Bambus"]
 
 print(mydb.list_collection_names())
 
-for x in mycol.find():
+for x in Bambus.find():
   print(x)
