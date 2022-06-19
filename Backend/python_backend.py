7a6a6c0ab1d5e99144849a223f3bc3d6e7a45d8a
Marko <mfrankovi@student.unipu.hr>
2022-06-19 14:31:56 +0200

    Update python_backend.py

--- Backend/python_backend.py
+++ Backend/python_backend.py
@@ -149,5 +149,5 @@ def dodavanje_komentara():
    return data
 
 if __name__ == '__main__':
-   app.run(port=int(os.environ.get("PORT",5000)))
+   app.run(host="0.0.0.0",port=int(os.environ.get("PORT",5000)))
    
